694bcb4e5477e537d78d3f7456ee2e582365f72a
Juan Diego Toro <jtorofranco36@gmail.com>
2024-03-09 18:42:04 -0600

    Agregue los comentarios

--- Problema_4/main.cpp
+++ Problema_4/main.cpp
@@ -24,6 +24,8 @@ int converNumero(char caracteres[]){
     int unsigned cont = 0, num=0;
 
     while(caracteres[cont]!='\0'){
+        //El valor de '0' es el numero al que equivale el cero en ascii y el valor de caracteres[cont] tambien es el valor del caracter en ascii
+        //La resta entre caracteres es la resta entre los valores en ascii
         num = num*10 + caracteres[cont]-'0';
         cont++;
     }
