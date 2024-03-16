873847d5d4fbe780ece3220fdcb43b702a47e8cf
Juan Diego Toro <jtorofranco36@gmail.com>
2024-03-16 17:32:00 -0600

    Arrglando problema de espacios en la funcion

--- Problema_2/main.cpp
+++ Problema_2/main.cpp
@@ -8,12 +8,14 @@ int main()
     char arr[20];
     char letra;
 
+    //Este for me genera el arreglo aleatorio
     for(unsigned int i = 0; i < 20; ++i){
         //El 65 representa el valor de 'A' en ascii
         letra = rand() % 26 + 65;
         arr[i] = letra;
     }
 
+    //Este for me muestra los valores en el arreglo
     for(unsigned int j = 0; j < 20; ++j){
         cout<<arr[j];
     }
@@ -29,13 +31,10 @@ int main()
 
 
 unsigned int cantLetra (char letra, char arr[]){
-
     unsigned int cont = 0;
 
     for(unsigned int i = 0; i < 20; ++i){
-
         if(arr[i] == letra) cont++;
-
     }
 
     return cont;
