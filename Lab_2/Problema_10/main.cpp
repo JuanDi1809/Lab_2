585a9b9f64f00cd6b1bb2126cab71afa433335df
Juan Diego Toro <jtorofranco36@gmail.com>
2024-03-16 17:11:21 -0600

    Cambiando tamños del arreglo y nombre de variables

--- Lab_2/Problema_10/main.cpp
+++ Lab_2/Problema_10/main.cpp
@@ -7,19 +7,18 @@ int main()
 {
     char letras[] = {'M','D','C','L','X','V','I'};
     unsigned int valorLetras[] = {1000, 500, 100, 50, 10, 5, 1};
-    unsigned int cont = 0, numArabigo = 0, num1, num2;
-    char numRomano[25];
+    unsigned int pos = 0, numArabigo = 0, num1, num2;
+    char numRomano[15];
 
     cout<<"Ingrese un numero romano: ";
-    cin.getline(numRomano, 30);
+    cin.getline(numRomano, 15);
 
-    while( numRomano[cont] != '\0'){
+    while( numRomano[pos] != '\0'){
 
         //Hago esta validacion porque la comparacion de si se restan o se suman es cada 2 numeros y la posicon de el segundo numero cada 2 es impar
-        if(cont % 2 != 0){
-
-            num1 = valorPorLetra(letras, valorLetras, numRomano[cont-1]);
-            num2 = valorPorLetra(letras, valorLetras, numRomano[cont]);
+        if(pos % 2 != 0){
+            num1 = valorPorLetra(letras, valorLetras, numRomano[pos-1]);
+            num2 = valorPorLetra(letras, valorLetras, numRomano[pos]);
 
             if(num2 <= num1){
                 numArabigo += num1 + num2;
@@ -28,20 +27,18 @@ int main()
                 //En este caso volteo la opreacion ya que num2 va a ser mas grande que num1
                 numArabigo += num2 - num1;
             }
-
         }
 
-        cont++;
+        pos++;
     }
 
-    if(cont % 2 == 0){
+    if(pos % 2 == 0){
         cout<<numArabigo;
     }
     else{
         //En caso de que el numero romano no sea una cantidad de letras pares, haria falta sumar el valor de la ultima letra
         //Cont-1 lo hago porque en el cont va a ser una posicion mas de la utlima en el numero romano
-        cout<<numArabigo +  valorPorLetra(letras, valorLetras, numRomano[cont-1]);
-
+        cout<<numArabigo +  valorPorLetra(letras, valorLetras, numRomano[pos-1]);
     }
 
     return 0;
